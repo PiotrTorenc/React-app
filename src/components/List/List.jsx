0cf3c5e128304e4b3bbb5cdb612111a202f51fcd
PiotrTorenc <piotr.torenc@gmail.com>
2022-10-27 20:56:16 +0200

    add first components

---
+++ src/components/List/List.jsx
@@ -0,0 +1,20 @@
+const List = () => {
+  return (
+    <div>
+      <header>
+        <h2>
+          Things to do<span>soon</span>
+        </h2>
+      </header>
+      <p>Interesting things I want to check out</p>
+      <section>
+        <article>
+          <h1>Books</h1>
+          <h1>Movies</h1>
+          <h1>Games</h1>
+        </article>
+      </section>
+    </div>
+  );
+};
+export default List;
