1baa228e2a81aca9f7a0f9819d7895d58068c0b5
PiotrTorenc <piotr.torenc@gmail.com>
2022-11-15 03:39:52 +0100

    add selectors and creators

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,24 @@
 import { createStore } from 'redux';
 import initialState from './initialstate';
 import shortid from 'shortid';
+import strContains from '../utils/strContains';
+
+export const getFilteredCards = ({ cards, searchString }, columnId) => {
+  return cards.filter(
+    (card) =>
+      card.columnId === columnId && strContains(card.title, searchString)
+  );
+};
+
+export const getAllColumns = (state) => state.columns;
+
+export const addColumn = (payload) => ({ type: 'ADD_COLUMN', payload });
+export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
+export const updateSearchString = (payload) => ({
+  type: 'UPDATE_SEARCHSTRING',
+  payload,
+});
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'ADD_COLUMN':
@@ -27,6 +45,8 @@ const reducer = (state, action) => {
           },
         ],
       };
+    case 'UPDATE_SEARCHSTRING':
+      return { ...state, searchString: action.payload };
     default:
       return state;
   }
