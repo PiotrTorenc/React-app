c72fbde67f1f18f66c13b6512ee1b4a4666cbf01
PiotrTorenc <piotr.torenc@gmail.com>
2022-11-16 23:45:35 +0100

    add subpages and 404

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
 import Container from './components/Container/Container';
+import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
+    <main>
+      <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/favorite' element={<Favorite />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
       </Container>
-    </div>
+    </main>
   );
 };
 
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,7 @@
+import PageTitle from '../PageTitle/PageTitle';
+
+const PageNotFound = () => {
+  return <PageTitle>404 Page not found</PageTitle>;
+};
+
+export default PageNotFound;
